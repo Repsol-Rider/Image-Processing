32340fb861cb24635b1c728bf481a9cc4fd81832
Rakesh <39953433+Repsol-Rider@users.noreply.github.com>
2021-03-20 22:56:20 +0530

    Code Updated (working without error)

--- 3-Changing-CRS-For-Output-TIFF-Files/change_crs.r
+++ 3-Changing-CRS-For-Output-TIFF-Files/change_crs.r
@@ -1,29 +1,37 @@
 library(raster)
+library(sf)
 
-dir <- "E:/GIS/INDICES"
+dir <- "D:/JOB/GIS"
 indice_list <- list.dirs(dir,recursive=FALSE,full.names=FALSE)
-copy_dir <- "E:/GIS/INDICES/CRS_CHANGED/"
+copy_dir <- paste(dir,"CRS_CHANGED/",sep="/")
 dir.create(copy_dir,showWarnings=FALSE,recursive=FALSE,mode=0777)
 main_indices <- c("ndvi","ndwi","savi")
 for(indice in indice_list){
-    if(indice %in% main_indices ){
-        indice_dir <- paste(dir,indice,sep="/")
-        dir.create(paste(copy_dir,indice,sep="/"),recursive=FALSE,mode=0777,showWarnings=FALSE)
-        indice_dir_list <- list.files(indice_dir,pattern="\\.tif$",full.names=TRUE)
-        for(a_file in indice_dir_list){
-            indice_one_file_name <- substr(a_file,nchar(indice_dir)+2,nchar(a_file))
-            r <- raster(a_file)
-            crs(r) <- "+proj=longlat +zone=43 +datum=WGS84 +units=m +no_defs"
-            proj4string(r) <- crs("+proj=longlat +zone=43 +datum=WGS84 +units=m +no_defs")
-            writeRaster(r,
-                        filename=paste(copy_dir,paste(indice,indice_one_file_name,sep="/"),sep=""),
-                        formate="GTiff",
-                        datatype="FLT4S",
-                        overwrite=TRUE
-            )
-            cat(paste("Projections for ",indice_one_file_name,": "))
-            cat(projection(raster(paste(paste(copy_dir,indice,sep=""),indice_one_file_name,sep="/")),asText=TRUE),"\n")
+  if(indice %in% main_indices ){
+    indice_dir <- paste(dir,indice,sep="/")
+    dir.create(paste(copy_dir,indice,sep="/"),recursive=FALSE,mode=0777,showWarnings=FALSE)
+    indice_dir_list <- list.files(indice_dir,pattern="\\.tif$",full.names=TRUE)
+    for(a_file in indice_dir_list){
+      indice_one_file_name <- substr(a_file,nchar(indice_dir)+2,nchar(a_file))
+      r <- raster(a_file)
+      r <- st_transform(r,"+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+      r2 <- projectRaster(r,crs="+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs")
+      writeRaster(r2,
+                  filename=paste(copy_dir,paste(indice,indice_one_file_name,sep="/"),sep=""),
+                  overwrite=TRUE
+      )
+      rtempdir <- paste(tempdir(),"raster",sep="\\")
+      if(dir.exists(rtempdir)){
+        rtempdir_files <- list.files(rtempdir,pattern="\\.gri$")
+        for(temp_file in rtempdir_files){
+          unlink(temp_file,recurisive=FALSE,force=FALSE)
         }
+      }else{
+        print("Raster Directory Doesn't exist!")
+      }
+      cat(paste("Projections for ",indice_one_file_name,": "))
+      cat(projection(raster(paste(paste(copy_dir,indice,sep=""),indice_one_file_name,sep="/")),asText=TRUE),"\n")
     }
-    cat("\n")
+  }
+  cat("\n")
 }

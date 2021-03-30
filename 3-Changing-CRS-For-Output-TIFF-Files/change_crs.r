5f99487846e5c8db1609c3a3c1c946eb23b885dc
pandumudiraj <80211605+pandumudiraj@users.noreply.github.com>
2021-03-30 18:21:54 +0530

    Raster Error resolved

--- 3-Changing-CRS-For-Output-TIFF-Files/change_crs.r
+++ 3-Changing-CRS-For-Output-TIFF-Files/change_crs.r
@@ -26,7 +26,8 @@ for(indice in indice_list){
         print("Raster Directory Doesn't exist!")
       }
       cat(paste("Projections for ",indice_one_file_name,": \n"))
-      cat(raster(paste(paste(copy_dir,indice,sep=""),indice_one_file_name,sep="/")),"\n ------------------------------------ \n")
+      ras <- raster(paste(paste(copy_dir,indice,sep=""),indice_one_file_name,sep="/"))
+      cat(ras,"\n ------------------------------------ \n")
     }
   }
   cat("\n")

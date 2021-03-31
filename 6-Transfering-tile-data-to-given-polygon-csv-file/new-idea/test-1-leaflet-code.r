e8f207fdc4f12e008d418deb4bebbf2fca1c6a0f
Rakesh <39953433+Repsol-Rider@users.noreply.github.com>
2021-03-31 11:34:37 +0530

    Half Done

--- 6-Transfering-tile-data-to-given-polygon-csv-file/new-idea/test-1-leaflet-code.r
+++ 6-Transfering-tile-data-to-given-polygon-csv-file/new-idea/test-1-leaflet-code.r
@@ -1,40 +1,55 @@
-library('leaflet')
+library('raster')
+
+options(warn=-1,max.print=10)
 
 # GETTING LONGITUDES AND LATITUDES FROM A GIVEN CSV FILE
-csv <- read.csv(file="D:/JOB/GIS/polygon.csv",nrows=1,stringsAsFactors=FALSE)
+csv <- read.csv(file="E:/GIS/polygon.csv",nrows=1,stringsAsFactors=FALSE)
 one_row <- csv[1]
 csv_len <- nchar(one_row)
 csv_sub_str <- substr(one_row,11,csv_len-2)
 whole_chr <- ""
 lat_longs_list <- c()
 for(chr in 1:nchar(csv_sub_str)){
-  one_chr <- substr(csv_sub_str,chr,chr)
-  if(one_chr == "," || one_chr == " "){
-    one_chr <- ""
-  }
-  whole_chr <- paste(whole_chr,one_chr,sep="")
-  if(nchar(whole_chr) == 16){
-    lat_longs_list <- append(lat_longs_list,whole_chr,after=length(lat_longs_list))
-    whole_chr <- ""
-  }
+    one_chr <- substr(csv_sub_str,chr,chr)
+    if(one_chr == "," || one_chr == " "){
+        one_chr <- ""
+    }
+    whole_chr <- paste(whole_chr,one_chr,sep="")
+    if(nchar(whole_chr) == 16){
+        lat_longs_list <- append(lat_longs_list,whole_chr,after=length(lat_longs_list))
+        whole_chr <- ""
+    }
 }
 csv_sub_str <- gsub(" ",",",csv_sub_str)
 latt <- c()
 longg <- c()
 for(val in 1:length(lat_longs_list)){
-  if(val %% 2 == 0){
-    latt <- append(latt,lat_longs_list[val],after=length(latt))
-  }else if(val %% 2 == 1){
-    longg <- append(longg,lat_longs_list[val],after=length(longg))
-  }
+    if(val %% 2 == 0){
+        latt <- append(latt,lat_longs_list[val],after=length(latt))
+    }else if(val %% 2 == 1){
+        longg <- append(longg,lat_longs_list[val],after=length(longg))
+    }
 }
-#PLOT POLYGON ON MAP
+#PLOT POLYGON WITH NDVI RASTER
 
-m1 <- matrix(as.numeric(strsplit(csv_sub_str,",")[[1]]),ncol=2,byrow=TRUE)
-map <- leaflet()
-map <- addTiles(map)
-map <- addPolygons(map,data=m1,color="blue",weight=4,smoothFactor=0.5,opacity=1.0,fillOpacity=0.5,fillColor='red')
-map
+ndvi <- raster("E:/GIS/DOWNLOADED_43PHR/INDICES/ndvi/reprojected/reprojetced_2/_T43PHR_20200925T050701.tif")
+new_extent <- extent(min(longg),max(longg),min(latt),max(latt))
+poly <- ndvi
+poly <- setExtent(ndvi,new_extent,keepres=TRUE)
+plot(poly)
+dimen <- dim(poly)
+num_of_pixels <- dimen[1]*dimen[2]
+cat("Read",num_of_pixels,"Pixels in the given polygon")
+ndvi
+raster(poly)
+pts <- rasterToPoints(poly,spatial=TRUE)
+data <- data.frame(pts)
+data[1]
+# m1 <- matrix(as.numeric(strsplit(csv_sub_str,",")[[1]]),ncol=2,byrow=TRUE)
+# map <- leaflet()
+# map <- addTiles(map)
+# map <- addPolygons(map,data=m1,color="blue",weight=4,smoothFactor=0.5,opacity=1.0,fillOpacity=0.5,fillColor='red')
+# map
 
 #PLOTTING LONG LATS ON SPECIFIED TILE GIVEN IN CSV FILE
 
--- 6-Transfering-tile-data-to-given-polygon-csv-file/new-idea/test-v3.r
+++ 6-Transfering-tile-data-to-given-polygon-csv-file/new-idea/test-v3.r
@@ -1,15 +1,20 @@
 library(raster)
 # library(zoom)
 
-r1 <- raster("D:/JOB/GIS/CRS_CHANGED/ndvi/T43PHR_20180906T050651.tif")
+ndvi <- raster("D:/INDICES/ndvi/reprojected/_T43PHR_20180906T050651.tif")
 new_extent <- extent(78.0841966200489,78.0983266241176,14.0392004877398,14.0497157354592)
-r2 <- setExtent(r1,new_extent,keepres=TRUE)
+poly <- ndvi
+poly <- setExtent(ndvi,new_extent,keepres=TRUE)
+plot(poly)
+dimen <- dim(poly)
+num_of_pixels <- dimen[1]*dimen[2]
+cat("Read",num_of_pixels,"Pixels in the given polygon")
+ndvi
+raster(poly)
+values(poly)
 
-raster(r2)
-plot(r2)
-
-# em <- merge(extent(r1),extent(r2))
+# em <- merge(extent(ndvi),extent(poly))
 # plot(em,type="n")
-# # plot(r1,add=TRUE,legend=FALSE)
-# plot(r2,add=TRUE,legend=FALSE,col="darkblue")
+# # plot(ndvi,add=TRUE,legend=FALSE)
+# plot(poly,add=TRUE,legend=FALSE,col="darkblue")
 # # zm()

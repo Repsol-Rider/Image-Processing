b572264b9088fc02dc37c22d44531a4eab8e5fb9
Rakesh <39953433+Repsol-Rider@users.noreply.github.com>
2021-04-02 16:47:13 +0530

    Update py.py

--- 1-Download-Sentinel-2-Images-Using-SentinelHubAPI/py.py
+++ 1-Download-Sentinel-2-Images-Using-SentinelHubAPI/py.py
@@ -2,15 +2,17 @@ from sentinelsat.sentinel import SentinelAPI
 from collections import OrderedDict
 import os
 
-os.chdir("E:/GIS/DOWNLOADED_43PHR/")
+os.chdir("D:/JOB/GIS/T43PHR/")
 username_esa = "porav43844"
 password_esa = "sourcecode88"
-tiles = ['43PHR']
+# username2 - ninadobrev
+# password2 - imageprocessing
+tiles = ['44QME']
 api = SentinelAPI(username_esa, password_esa, 'https://scihub.copernicus.eu/dhus')
 query_kwargs = {
         'platformname': 'Sentinel-2',
         'producttype': 'S2MSI1C',
-        'date': ('20200528','20211229'),
+        'date': ('20200528','20210328'),
         'cloudcoverpercentage': ('0', '30')}
 products = OrderedDict()
 for tile in tiles:
@@ -18,7 +20,6 @@ for tile in tiles:
     kw['tileid'] = tile
     pp = api.query(**kw)
     products.update(pp)
-
 print (products)
 api.download_all(products)
 

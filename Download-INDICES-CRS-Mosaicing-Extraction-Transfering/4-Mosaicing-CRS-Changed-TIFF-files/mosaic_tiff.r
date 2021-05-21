20704bb6c014e372af6d5ce229671a71cce2e293
Rakesh <39953433+Repsol-Rider@users.noreply.github.com>
2021-05-22 04:16:50 +0530

    Update mosaic_tiff.r

--- Download-INDICES-CRS-Mosaicing-Extraction-Transfering/4-Mosaicing-CRS-Changed-TIFF-files/mosaic_tiff.r
+++ Download-INDICES-CRS-Mosaicing-Extraction-Transfering/4-Mosaicing-CRS-Changed-TIFF-files/mosaic_tiff.r
@@ -1,14 +1,20 @@
 library(gdalUtils)
 library(raster)
-setwd("D:/JOB/GIS/TS/INDICES/CRS_CHANGED/ndvi/")
-gdalbuildvrt(gdalfile = "*.tif", # uses all tiffs in the current folder
-             output.vrt = "ndvi.vrt")
-gdal_translate(src_dataset = "ndvi.vrt", 
-               dst_dataset = "ndvi.tif", 
-               output_Raster = TRUE,
-               options = c("BIGTIFF=YES", "COMPRESSION=LZW"))
-unlink("ndvi.vrt",force=FALSE,recursive=FALSE)
-plot(raster("D:/JOB/GIS/TS/INDICES/CRS_CHANGED/ndvi/ndvi.tif"))
 
+indice_dir <- "D:/JOB/GIS/TILES_DATA/TS/INDICES/CRS_CHANGED/"
+indice_list <- c("ndvi")
+for(indice in indice_list){
+  setwd(paste(indice_dir,paste(indice,"",sep="/"),sep=""))
+  indice_file_vrt <- paste(indice,"vrt",sep=".")
+  indice_file_tif <- paste(indice,"tif",sep=".")
+  gdalbuildvrt(gdalfile = "*.tif", # uses all tiffs in the current folder
+               output.vrt = indice_file_vrt)
+  gdal_translate(src_dataset = indice_file_vrt, 
+                 dst_dataset = indice_file_tif, 
+                 output_Raster = TRUE,
+                 options = c("BIGTIFF=YES", "COMPRESSION=LZW"))
+  unlink(indice_file_vrt,force=FALSE,recursive=FALSE)
+  plot(raster(paste(indice_dir,indice_file_tif,sep="/")))
+}
 
 # https://stackoverflow.com/questions/50234139/using-mosaic-in-r-for-merge-multiple-geotiff

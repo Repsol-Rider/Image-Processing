a85edff4e1ee77b0f4a24ba636fbbe48d8ff42c3
Rakesh <39953433+Repsol-Rider@users.noreply.github.com>
2021-05-25 18:16:32 +0530

    Update py_v2.r

--- Download-INDICES-CRS-Mosaicing-Extraction-Transfering/1-Download-Sentinel-2-Images-Using-SentinelHubAPI/py_v2.r
+++ Download-INDICES-CRS-Mosaicing-Extraction-Transfering/1-Download-Sentinel-2-Images-Using-SentinelHubAPI/py_v2.r
@@ -1,18 +1,20 @@
 from sentinelsat import SentinelAPI
 from collections import OrderedDict
-import os
+import os,requests,html2text
 
-os.chdir("D:/JOB/GIS/TS/")
+h = html2text.HTML2Text()
+h.ignore_links = True
+os.chdir("D:/JOB/GIS/PIXEL_DATA/TS")
 # username2 - ninadobrev
 # password2 - imageprocessing
-api = SentinelAPI('porav43844', 'sourcecode88')
-tiles = ['43PHR']
-#'44QME','4EQHV','44QKF','44QLF','44QLG','44DMF','43QGU','43QHV','43QHB','43PHT','43QHU','44PKC'
-#,'44QKD','44QKE','44QKG','44QLD','44QLE','44QME','43PGT','43QGA','43QGB','44QMD','44QNE','43QHA'
+api = SentinelAPI('fake99', 'newpassword88','https://scihub.copernicus.eu/dhus')
+#tiles = ['43QHV','43QHA','43QHB','44QKM','44QKE']
+tiles = ['43QHA','43QHB','43QHU','43QHV','44QKD','44QKE','44QKF','44QKG','44QLD','44QLE','44QLF','44QLG','44QME','44QMF']
+
 query_kwargs = {
         'platformname': 'Sentinel-2',
         'producttype': 'S2MSI1C',
-        'date': ('20200528', '202103015'),
+        'date': ('20200915', '20201015'),
         'cloudcoverpercentage': ('0', '30')}
 
 products = OrderedDict()
@@ -21,5 +23,12 @@ for tile in tiles:
     kw['tileid'] = tile  # products after 2017-03-31
     pp = api.query(**kw)
     products.update(pp)
+print(len(products))
+
+# the below 3 lines are used for revoking offline products gonna take 2-3 days
+
+#for product in products:
+#    r = requests.get("https://scihub.copernicus.eu/dhus/odata/v1/Products('"+product+"')/Online/$value", auth=('fake99', 'newpassword88'))
+#    print(h.handle(r.text),sep="",end="")
 
 api.download_all(products)

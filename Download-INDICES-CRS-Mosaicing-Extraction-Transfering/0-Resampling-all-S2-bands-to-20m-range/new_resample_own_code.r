aa62bb085b0944ad6f664428ae2febb48ccf2fbd
Rakesh <39953433+Repsol-Rider@users.noreply.github.com>
2021-06-08 20:22:09 +0530

    Update new_resample_own_code.r

--- Download-INDICES-CRS-Mosaicing-Extraction-Transfering/0-Resampling-all-S2-bands-to-20m-range/new_resample_own_code.r
+++ Download-INDICES-CRS-Mosaicing-Extraction-Transfering/0-Resampling-all-S2-bands-to-20m-range/new_resample_own_code.r
@@ -3,5 +3,5 @@ library(sp)
 
 r <- raster("test_raster_10m_band.jp2")
 s <- raster("test_raster_20m_band.jp2")
-t <- resample(r, s, method='bilinear')
+s <- resample(r, s, method='bilinear')
 plot(t)
